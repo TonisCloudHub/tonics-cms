05b3ccb4ae48479867c5323149cfe59bb5627d81
Olayemi Faruq <olayemi@tonics.app>
2024-05-01 20:02:43 +0100

    Added WriTonics yml and fixed bug in script.sh

--- .github/bookworm/scripts/script.sh
+++ .github/bookworm/scripts/script.sh
@@ -40,7 +40,7 @@ sudo incus launch images:debian/bookworm/amd64 tonics-cms
 # Dependencies
 sudo incus exec tonics-cms -- bash -c "apt update -y && apt upgrade -y"
 
-sudo incus exec tonics-cms -- bash -c "DEBIAN_FRONTEND=noninteractive apt install -y unzip mariadb-server nginx wget php php8.2-fpm php8.2-dom php8.2-xml php8.2-xmlrpc php8.2-soap php8.2-mysql php8.2-mbstring php8.2-readline php8.2-gd  php8.2-gmp php8.2-bcmath php8.2-zip php8.2-curl php8.2-intl php8.2-apcu"
+sudo incus exec tonics-cms -- bash -c "DEBIAN_FRONTEND=noninteractive apt install -y unzip mariadb-server nginx wget php php8.2-fpm php8.2-dom php8.2-xml php8.2-cli php8.2-soap php8.2-mysql php8.2-mbstring php8.2-readline php8.2-gd php8.2-zip php8.2-gmp php8.2-bcmath php8.2-zip php8.2-curl php8.2-intl php8.2-apcu"
 
 # Setup MariaDB
 sudo incus exec tonics-cms -- bash -c "mysql --user=root -sf <<EOS
